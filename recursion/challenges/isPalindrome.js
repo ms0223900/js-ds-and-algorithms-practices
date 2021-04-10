8e9b21fa9f71f091f76fa6670275c45814a8376d
ms0223900 <ms0223900@gmail.com>
2021-04-10 20:35:36 +0800

    chore: add another isPalindrome sample

--- recursion/challenges/isPalindrome.js
+++ recursion/challenges/isPalindrome.js
@@ -4,6 +4,18 @@ const isPalindrome = (input='') => {
   return isPalindrome(input.slice(1, input.length - 1));
 }
 
+function isPalindrome(input='') {
+  if(input.length === 1) return true;
+  if(input.length === 2) return input[0] === input[1];
+  if(input[0] === input.slice(-1)) {
+    return isPalindrome(
+      input.slice(1, -1)
+    )
+  }
+  return false
+}
+
 console.log(
-  isPalindrome('tacocat')
+  isPalindrome('tacocat'),
+  isPalindrome('abcddcba'),
 )
